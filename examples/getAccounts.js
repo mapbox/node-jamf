efa1742a7e1a790e29375997e0804dbc251f6676
Alexandra Ulsh <ulsh@mapbox.com>
2017-06-01 18:46:28 -0400

    Switches from option to config for variable name

--- examples/getAccounts.js
+++ examples/getAccounts.js
@@ -1,13 +1,13 @@
 var jamfApi = require('../lib/api.js');
 
-var options = {
+var config = {
   user: process.env.JAMF_API_USER,
   password: process.env.JAMF_API_PASSWORD,
   jamfUrl: process.env.JAMF_URL,
   json: true
 }
 
-jamfApi.getAccounts(options, function (err, response){
+jamfApi.getAccounts(config, function (err, response){
   if (err) console.log(err);
   console.log(response);
 });
--- examples/getComputers.js
+++ examples/getComputers.js
@@ -1,13 +1,13 @@
 var jamfApi = require('../lib/api.js');
 
-var options = {
+var config = {
   user: process.env.JAMF_API_USER,
   password: process.env.JAMF_API_PASSWORD,
   jamfUrl: process.env.JAMF_URL,
   json: true
 }
 
-jamfApi.getComputers(options, function (err, response){
+jamfApi.getComputers(config, function (err, response){
   if (err) console.log(err);
   console.log(response);
 });
