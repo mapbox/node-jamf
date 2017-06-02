cbadc99fe1d09032d539274182ebf5d2f41f5133
Alexandra Ulsh <ulsh@mapbox.com>
2017-06-02 14:11:23 -0400

    Adds test for throw exception if Jamf API client is missing config

--- test/jamfclient.test.js
+++ test/jamfclient.test.js
@@ -2,3 +2,8 @@ var tape = require('tape');
 var nock = require('nock');
 
 var JamfApiClient = require('../lib/jamfclient.js');
+
+tape('Jamf client fails if config is not provided', function(assert){
+  assert.throws(function() { new JamfApiClient() });
+  assert.end();
+});
