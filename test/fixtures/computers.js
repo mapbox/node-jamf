e6bce3d112bc3ce97036a2ed5cd2d9c2ec30069d
Alexandra Ulsh <ulsh@mapbox.com>
2017-06-02 18:44:12 -0400

    Adds test for obtaining a list of all computers

---
+++ test/fixtures/computers.js
@@ -0,0 +1,11 @@
+module.exports.allComputers = allComputers;
+
+function allComputers() {
+  var allComputers = { computers:
+   [ { id: 1, name: 'MacBook Air' },
+     { id: 2, name: 'iMac' },
+     { id: 3, name: 'Alex’s Macbook Air' },
+     { id: 4, name: 'Ian’s Macbook Pro' },
+     { id: 5, name: 'Olivia’s iMac' } ] }
+  return allComputers;
+}
