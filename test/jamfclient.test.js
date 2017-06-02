8dcaa018dcc1b134b2c13e1cfbd62e11b16be49f
Alexandra Ulsh <ulsh@mapbox.com>
2017-06-02 14:31:41 -0400

    Add test for successful Jamf API client configuration

--- test/jamfclient.test.js
+++ test/jamfclient.test.js
@@ -3,7 +3,19 @@ var nock = require('nock');
 
 var JamfApiClient = require('../lib/jamfclient.js');
 
-tape('Jamf client fails if config is not provided', function(assert){
+var testConfig = {
+  user: 'fakeuser',
+  password: 'fakepassword',
+  jamfUrl: 'https://abc123.jamfcloud',
+  format: 'json'
+}
+
+tape('Jamf API client fails if config is not provided', function(assert){
   assert.throws(function() { new JamfApiClient() });
+  assert.end();
+});
+
+tape('Jamf API client configuration success', function(assert){
+  assert.doesNotThrow(function() { new JamfApiClient(testConfig) });
   assert.end();
 });
