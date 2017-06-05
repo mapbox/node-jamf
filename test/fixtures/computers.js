a7d4076f7b076298a7c15ed92bed65f0f25ab8d4
Alexandra Ulsh <ulsh@mapbox.com>
2017-06-05 12:00:14 -0400

    Adds text and fixtures for XML data

--- test/fixtures/computers.js
+++ test/fixtures/computers.js
@@ -1,4 +1,5 @@
 module.exports.allComputers = allComputers;
+module.exports.xmlAllComputers = xmlAllComputers;
 
 function allComputers() {
   var allComputers = { computers:
@@ -8,4 +9,9 @@ function allComputers() {
      { id: 4, name: 'Ian’s Macbook Pro' },
      { id: 5, name: 'Olivia’s iMac' } ] }
   return allComputers;
+}
+
+function xmlAllComputers() {
+  var xmlAllComputers = '<?xml version="1.0" encoding="UTF-8"?><computers><size>6</size><computer><id>1</id><name>MacBook Air</name></computer><computer><id>2</id><name>iMac</name></computer><computer><id>3</id><name>Alex’s Macbook Air</name></computer><computer><id>4</id><name>Ian’s Macbook Pro</name></computer><computer><id>5</id><name>Olivia’s iMac</name></computer></computers>'
+  return xmlAllComputers;
 }
--- test/jamfclient.test.js
+++ test/jamfclient.test.js
@@ -3,13 +3,22 @@ var nock = require('nock');
 
 var JamfApiClient = require('../lib/jamfclient.js');
 
-var allComputers = require('./fixtures/computers.js').allComputers();
+var computerFixtures = require('./fixtures/computers.js');
+var allComputers = computerFixtures.allComputers();
+var xmlAllComputers = computerFixtures.xmlAllComputers();
 
-var testConfig = {
+var jsonConfig = {
   user: 'fakeuser',
   password: 'fakepassword',
   jamfUrl: 'https://abc123.jamfcloud.com',
   format: 'json'
+}
+
+var xmlConfig = {
+  user: 'fakeuser',
+  password: 'fakepassword',
+  jamfUrl: 'https://abc123.jamfcloud.com',
+  format: 'xml'
 }
 
 tape('Jamf API client fails if config is not provided', function(assert){
@@ -18,21 +27,38 @@ tape('Jamf API client fails if config is not provided', function(assert){
 });
 
 tape('Jamf API client configuration success', function(assert){
-  assert.doesNotThrow(function() { new JamfApiClient(testConfig) });
+  assert.doesNotThrow(function() { new JamfApiClient(jsonConfig) });
   assert.end();
 });
 
-tape('Jamf API requests to get computers', function(assert){
+tape('Jamf API request to get computers in JSON format', function(assert){
 
   nock('https://abc123.jamfcloud.com')
     .get('/JSSResource/computers')
     .reply('200', allComputers);
 
-  var jamf = new JamfApiClient(testConfig);
+  var jamf = new JamfApiClient(jsonConfig);
 
   jamf.get('/computers', function (err, res){
     assert.ifErr(err, 'Function does not error');
-    assert.deepEqual(res, allComputers, 'Function returns a list of all computers');
+    assert.deepEqual(res, allComputers, 'Function returns a list of all computers in JSON');
+  });
+
+  assert.end();
+
+});
+
+tape('Jamf API request to get computers in XML format', function(assert){
+
+  nock('https://abc123.jamfcloud.com')
+    .get('/JSSResource/computers')
+    .reply('200', xmlAllComputers);
+
+  var jamf = new JamfApiClient(xmlConfig);
+
+  jamf.get('/computers', function (err, res){
+    assert.ifErr(err, 'Function does not error');
+    assert.deepEqual(res, xmlAllComputers, 'Function returns a list of all computers in XML');
   });
 
   assert.end();
